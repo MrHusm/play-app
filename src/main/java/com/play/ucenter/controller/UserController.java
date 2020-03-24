3a126caf7bf0a2bef0b6f6674c0530eef2a8e8c4
hushengmeng <hushengmeng>
2020-03-24 18:34:22 +0800

    init

--- src/main/java/com/play/ucenter/controller/UserController.java
+++ src/main/java/com/play/ucenter/controller/UserController.java
@@ -2,7 +2,6 @@ package com.play.ucenter.controller;
 
 import com.play.base.controller.BaseController;
 import com.play.base.exception.ServiceException;
-import com.play.base.utils.ResultMessage;
 import com.play.base.utils.ResultResponse;
 import com.play.ucenter.model.User;
 import com.play.ucenter.service.IUserService;
@@ -11,13 +10,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 /**
@@ -27,6 +25,7 @@ import java.util.Date;
 @Controller
 @Scope("prototype")
 @RequestMapping("user")
+@Validated
 public class UserController extends BaseController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
@@ -42,7 +41,7 @@ public class UserController extends BaseController {
      */
     @ResponseBody
     @RequestMapping("/loginByMobile")
-    public ResultResponse loginByMobile(@NotBlank String token) {
+    public ResultResponse loginByMobile(String token) {
         User user = new User();
         user.setName("我的");
         user.setCreateDate(new Date());
@@ -54,13 +53,12 @@ public class UserController extends BaseController {
      * 用户详情
      */
     @RequestMapping("/info")
-    public String info(@NotBlank String token, Model model) throws ServiceException {
+    public String info(@RequestParam(required = true) String token, Model model) throws ServiceException {
         User user = new User();
         user.setName("我的");
         user.setCreateDate(new Date());
         model.addAttribute("user", user);
-        throw new ServiceException(ResultMessage.F4006);
-//        return "ucenter/info";
+        return "ucenter/info";
     }
 
 }
